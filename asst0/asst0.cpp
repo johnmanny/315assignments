ba3aa81fbf076733e3cb4ce83ee683a5132a482f
recuxer <ooowea@gmail.com>
2018-01-08 13:17:36 -0800

    initial commit

---
+++ asst0/asst0.cpp
@@ -0,0 +1,26 @@
+/*
+	Author: John Nemeth
+	Description: todo
+	Sources: todo
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	const int SPACESNUM = 1, DISTTONEWLINE = 1, FIRSTLINE = 2;
+	int x, y = 0;
+	cin.ignore(FIRSTLINE, '\n');
+	while (cin) {
+		cin >> x;
+		cin.ignore(SPACESNUM, ' ');
+		cin >> y;
+		cin.ignore(DISTTONEWLINE, '\n');
+		
+		cout << x << ' ' << y << endl;
+	}
+
+
+	return 0;
+}
