6b0c9a2bd544467940143544912596f3f8a17680
recuxer <ooowea@gmail.com>
2018-01-12 16:21:35 -0800

    added comments

--- asst0/asst0.cpp
+++ asst0/asst0.cpp
@@ -8,17 +8,19 @@
 using namespace std;
 
 int main() {
-	const int SPACESNUM = 1, DISTTONEWLINE = 1;
+	const int SPACESNUM = 1, DISTTONEWLINE = 10;
 	int x, y, lines = 0;
+
+	//read number of lines and move to next line
 	cin >> lines;
 	cin.ignore(DISTTONEWLINE, '\n');
-	
+
+	//loop until lines finished
 	while (lines != 0) {
 		cin >> x;
 		cin.ignore(SPACESNUM, ' ');
 		cin >> y;
 		cin.ignore(DISTTONEWLINE, '\n');
-		
 		cout << x + y << ' ' << x * y << endl;
 		lines--;
 	}
